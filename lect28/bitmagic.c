4752e2bff42954fc77a935a6a183bae43a9ad833
utk4546 <91utkarsh@gmail.com>
2025-10-28 15:49:55 +0530

    bitmagic

---
+++ lect28/bitmagic.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ 
+    int n;
+    printf("enter a number ");
+    scanf("%d",&n);
+    if(n&1)
+    { 
+        printf("odd");
+    }else
+    {
+      printf("even");
+    }
+    return 0;
+}
