22c786f3e532a88d0feb67d77a4e2b8b7d017bbf
utk4546 <91utkarsh@gmail.com>
2025-12-29 09:50:00 +0530

    stack

---
+++ lect31/program to convert the infix expression to postfix expression..c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <ctype.h>
+
+char stack[50];
+int top = -1;
+
+void push(char x){
+    stack[++top] = x;
+}
+
+char pop(){
+    return stack[top--];
+}
+
+int priority(char x){
+    if(x=='+' || x=='-')
+        return 1;
+    if(x=='*' || x=='/')
+        return 2;
+    return 0;
+}
+
+int main(){
+    char exp[50], *e, x;
+    printf("Enter infix expression: ");
+    scanf("%s", exp);
+
+    e = exp;
+    printf("Postfix expression: ");
+
+    while(*e != '\0'){
+        if(isalnum(*e))
+            printf("%c", *e);
+        else if(*e == '(')
+            push(*e);
+        else if(*e == ')'){
+            while((x = pop()) != '(')
+                printf("%c", x);
+        }
+        else{
+            while(priority(stack[top]) >= priority(*e))
+                printf("%c", pop());
+            push(*e);
+        }
+        e++;
+    }
+
+    while(top != -1)
+        printf("%c", pop());
+
+    return 0;
+}
